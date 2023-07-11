02779d3f382b5af201a6cb4741d03727f4b0a33b
Tembeon <tembeon@gmail.com>
2023-07-11 18:09:44 +0300

    initial

---
+++ bin/lfg_bot.dart
@@ -0,0 +1,5 @@
+import 'package:lfg_bot/lfg_bot.dart' as lfg_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lfg_bot.calculate()}!');
+}
---
+++ test/lfg_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:lfg_bot/lfg_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
