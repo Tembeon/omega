c5e4e0aca604db2c9ed8e2da7703f93d4427ca3f
Tembeon <tembeon@gmail.com>
2023-07-14 22:42:36 +0300

    add exceptions for bot

---
+++ lib/features/commands/command_exceptions.dart
@@ -0,0 +1,31 @@
+/// Exception thrown when a command is executed with too many players.
+class TooManyPlayers implements Exception {}
+
+/// Exception thrown when a creator tries to leave their own activity.
+class CreatorCannotLeave implements Exception {}
+
+/// Exception thrown when a player tries to join an activity they are already in.
+class AlreadyJoined implements Exception {}
+
+/// Exception thrown when a player tries to leave an activity they are not in.
+class NotJoined implements Exception {}
+
+/// Exception thrown when a command is executed by a player who is not the creator.
+class NotCreator implements Exception {}
+
+/// Exception thrown when a command triggered, but bot refused to respond.
+class CantRespond implements Exception {
+  const CantRespond(this.reason);
+
+  /// Reason why the bot can't respond.
+  final String reason;
+
+  String toHumanMessage() {
+    final sb = StringBuffer();
+    sb.writeln('Невозможно выполнить команду.');
+    sb.writeln('Причина: $reason');
+    sb.writeln('Попробуйте ещё раз или обратитесь к администрации сервера.');
+
+    return sb.toString();
+  }
+}
