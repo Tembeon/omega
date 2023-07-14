c82acf8a36e01dc22964927444426a54f3e57a73
Tembeon <tembeon@gmail.com>
2023-07-14 19:48:07 +0300

    add bot initialization

--- bin/lfg_bot.dart
+++ bin/lfg_bot.dart
@@ -1,5 +1,28 @@
-import 'package:lfg_bot/lfg_bot.dart' as lfg_bot;
+import 'dart:async';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${lfg_bot.calculate()}!');
+import 'package:io/io.dart';
+import 'package:lfg_bot/core/bot/nyxx_core.dart';
+import 'package:lfg_bot/core/bot/nyxx_interactions.dart';
+import 'package:lfg_bot/core/utils/config_data_loader.dart';
+import 'package:lfg_bot/core/utils/flavors.dart';
+
+void main(List<String> arguments) => runZonedGuarded(
+      runner,
+      (error, stack) {
+        if (error is ConfigFileMissing) {
+          exit(ExitCode.config.code);
+        }
+
+        exit(ExitCode.software.code);
+      },
+    );
+
+void runner() {
+  // load bot settings
+  Flavors.readFromFile(useTestEnvironment: true);
+
+  // initialize bot
+  final bot = BotCore().bot;
+  final interactions = BotInteractions(bot).interactions;
 }
