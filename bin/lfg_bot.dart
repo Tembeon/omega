d8d4aff1375d9c4c7f0ca87d41de0d76bb5db4c2
Tembeon <tembeon@gmail.com>
2023-07-14 22:40:56 +0300

    load config data on startup

--- bin/lfg_bot.dart
+++ bin/lfg_bot.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:io/io.dart';
+import 'package:l/l.dart';
 import 'package:lfg_bot/core/bot/nyxx_core.dart';
 import 'package:lfg_bot/core/bot/nyxx_interactions.dart';
 import 'package:lfg_bot/core/utils/config_data_loader.dart';
@@ -11,9 +12,12 @@ void main(List<String> arguments) => runZonedGuarded(
       runner,
       (error, stack) {
         if (error is ConfigFileMissing) {
+          l.e(error.toString());
           exit(ExitCode.config.code);
         }
 
+        print(stack);
+        l.e(error.toString(), stack);
         exit(ExitCode.software.code);
       },
     );
@@ -21,6 +25,7 @@ void main(List<String> arguments) => runZonedGuarded(
 void runner() {
   // load bot settings
   Flavors.readFromFile(useTestEnvironment: true);
+  ConfigDataLoader().load();
 
   // initialize bot
   final bot = BotCore().bot;
