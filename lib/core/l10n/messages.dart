26d787ee681125e38ddb8566b8ba79f4006cac2c
Tembeon <tembeon@gmail.com>
2023-10-29 20:59:45 +0300

    [WIP] update localization: migrate to intl

---
+++ lib/core/l10n/messages.dart
@@ -0,0 +1,62 @@
+import 'package:intl/intl.dart';
+
+/// Used in command option as a key for name
+String commandOptionNameKey = Intl.message(
+  'название',
+  name: 'commandOptionNameKey',
+  desc: 'A name of parameter in command, which visible for user',
+);
+
+String commandOptionNameDescription = Intl.message(
+  'Введите название активности',
+  name: 'commandOptionNameDescription',
+  desc: 'A description text for "name" option in command',
+);
+
+String commandOptionDescriptionKey = Intl.message(
+  'описание',
+  name: 'commandOptionDescriptionKey',
+  desc: 'A name of parameter in command, which visible for user',
+);
+
+String commandOptionDescriptionDescription = Intl.message(
+  'Введите описание активности',
+  name: 'commandOptionDescriptionDescription',
+  desc: 'A description text for "description" option in command',
+);
+
+String commandOptionDateKey = Intl.message(
+  'дата',
+  name: 'commandOptionDateKey',
+  desc: 'A name of parameter in command, which visible for user',
+);
+
+String commandOptionDateDescription = Intl.message(
+  'Введите дату начала активности [15 01 2023]',
+  name: 'commandOptionDateDescription',
+  desc: 'A description text for "date" option in command',
+);
+
+String commandOptionTimeKey = Intl.message(
+  'время',
+  name: 'commandOptionTimeKey',
+  desc: 'A name of parameter in command, which visible for user',
+);
+
+String commandOptionTimeDescription = Intl.message(
+  'Введите время начала активности [15 01]',
+  name: 'commandOptionTimeDescription',
+  desc: 'A description text for "time" option in command',
+);
+
+String commandOptionTimezoneKey = Intl.message(
+  'часовой_пояс',
+  name: 'commandOptionTimezoneKey',
+  desc: 'A name of parameter in command, which visible for user',
+);
+
+String commandOptionTimezoneDescription = Intl.message(
+  'Введите ваш текущий часовой пояс',
+  name: 'commandOptionTimezoneDescription',
+  desc: 'A description text for "timezone" option in command',
+);
