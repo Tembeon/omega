0cc5eac7e9c391052f69ffcffc468a9b0a464acd
Tembeon <tembeon@gmail.com>
2023-12-10 20:57:41 +0300

    update createRaidCommand handler to make test posts

--- lib/features/create/handler/create_handle.dart
+++ lib/features/create/handler/create_handle.dart
@@ -1,23 +1,49 @@
 import 'package:nyxx/nyxx.dart';
 
+import '../../../core/utils/context/context.dart';
+import '../../../core/utils/loaders/bot_settings.dart';
 import '../../command_manager/command_manager.dart';
+import '../../lfg_manager/data/models/register_activity.dart';
+import '../../lfg_manager/lfg_manager.dart';
 
 CommandCreator createRaidCommand() {
   return (
-    builder: createCommandBuilder,
-    handler: _handler,
+    builder: _createBuilder,
+    handler: _createHandler,
   );
 }
 
-Future<void> _handler(InteractionCreateEvent<ApplicationCommandInteraction> interaction) {
-  return interaction.interaction.respond(
-    MessageBuilder(
-      content: 'Raid created',
+Future<void> _createHandler(InteractionCreateEvent<ApplicationCommandInteraction> interaction) async {
+  final user = interaction.interaction.member?.id;
+  print('User "$user" is trying to create new LFG post');
+  if (user == null) return; // refuse to work with bots
+
+  final manager = Context.root.get<LFGManager>('manager');
+  final settings = Context.root.get<BotSettings>('settings');
+
+  // create command always has 1 subcommand: raid, dungeon, custom.
+  // So we can just use first to get options of subcommand.
+  // All options for `/create` command are equal for all subcommands.
+  final createOptions = interaction.interaction.data.options!.first.options!;
+
+  final name = createOptions.firstWhere((e) => e.name == 'название').value as String;
+  final description = createOptions.firstWhere((e) => e.name == 'описание').value as String;
+
+  final activity = settings.activityData.activities.where((e) => e.name == name).first;
+
+  print('Creating new LFG post for user "$user" with activity "$name" and description "$description"');
+  await manager.create(
+    interaction: interaction,
+    builder: LFGPostBuilder.fromActivity(
+      activity: activity,
+      authorID: user,
+      description: description,
+      unixDate: 1702933200000,
     ),
   );
 }
 
-ApplicationCommandBuilder createCommandBuilder() {
+ApplicationCommandBuilder _createBuilder() {
   return ApplicationCommandBuilder(
     name: 'create',
     description: 'Создать активность',
