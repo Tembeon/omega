56c1e1dd6efe6b69cb6654cdac680fce0ed3e7ca
Artur Rafikov <tembeon@gmail.com>
2024-07-17 17:36:54 +0300

    make bot private, add checks for channels

--- lib/features/promoter/promoter.dart
+++ lib/features/promoter/promoter.dart
@@ -1,47 +1,55 @@
 import 'package:nyxx/nyxx.dart';
 
 import '../../core/const/command_exceptions.dart';
-import '../../core/utils/services.dart';
 import '../lfg_manager/data/models/register_activity.dart';
+import '../settings/settings.dart';
 import 'promoter_messages.dart';
 
 /// {@template Promoter}
+///
 /// Promoter for notifying about new LFG posts.
+///
 /// {@endtemplate}
 class Promoter {
   /// {@macro Promoter}
   const Promoter({
-    required this.bot,
-  });
+    required NyxxGateway bot,
+  }) : _bot = bot;
 
-  final NyxxGateway bot;
+  final NyxxGateway _bot;
 
   /// Notifies about a new LFG post.
-  Future<void> notifyAboutLFG(LFGPostBuilder builder, Snowflake postId) async {
-    final settings = Services.i.settings;
+  Future<void> notifyAboutLFG(
+    LFGPostBuilder builder,
+    Snowflake postId,
+    Settings settings,
+  ) async {
     final promoChannel = await settings.getPromotesChannel();
     final lfgChannel = await settings.getLFGChannel();
+    if (promoChannel == null || lfgChannel == null) return;
 
-    final channel = await bot.channels.fetch(Snowflake(promoChannel!));
-    final postChannel = await bot.channels.fetch(Snowflake(lfgChannel!));
+    final channel = await _bot.channels.fetch(Snowflake(promoChannel));
+    final postChannel = await _bot.channels.fetch(Snowflake(lfgChannel));
+
     if (channel.type != ChannelType.guildText) {
       throw CantRespondException(
         'Not a promo channel.\n'
         'ID: $promoChannel',
       );
-    } else {
-      channel as GuildTextChannel;
-      final message = MessageTemplates.getRandomLFGMessage(
-        builder.name,
-        channel.guildId.toString(),
-        postChannel.id.toString(),
-        postId.toString(),
-      );
-      await channel.sendMessage(
-        MessageBuilder(
-          content: message,
-        ),
-      );
     }
+
+    channel as GuildTextChannel;
+    final message = MessageTemplates.getRandomLFGMessage(
+      builder.name,
+      channel.guildId.toString(),
+      postChannel.id.toString(),
+      postId.toString(),
+    );
+
+    await channel.sendMessage(
+      MessageBuilder(
+        content: message,
+      ),
+    );
   }
 }
